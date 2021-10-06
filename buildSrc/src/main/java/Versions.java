c03ca52f693c1b4b92a1ea275d4c4f94cc2732f5
xelay <xelay@narod.ru>
2021-10-06 23:55:53 +0300

    hw1

---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,30 @@
+public interface Versions {
+    String guava = "28.2-jre";
+    String jmh = "1.23";
+    String jol = "0.10";
+    String asm = "8.0.1";
+    String glassfishJson = "1.1.4";
+    String protobuf = "3.12.1";
+    String flyway = "6.4.4";
+    String testcontainers = "1.13.0";
+    String postgresql = "42.2.5";
+
+    String lombok = "1.18.12";
+    String gson = "2.8.6";
+
+
+    String mongodb = "4.1.0";
+    String mongodbReactive = "4.1.0";
+    String cassandra = "4.9.0";
+    String neo4j = "4.1.1";
+    String jedis = "3.3.0";
+
+    String jetty = "9.4.31.v20200723";
+    String freemarker = "2.3.30";
+    String reflections = "0.9.12";
+
+    String sockjs = "1.1.2";
+    String stomp = "2.3.3-1";
+    String bootstrap = "4.4.1-1";
+    String netty = "4.1.53.Final";
+}
